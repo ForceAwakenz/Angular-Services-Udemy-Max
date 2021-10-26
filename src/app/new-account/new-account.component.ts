4b723b526587f3f7431ea0d9fdd0f53211a648c1
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-26 09:08:07 +0300

    2 services already work

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountService.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
+  }
+}
